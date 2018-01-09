a9e735c4eff78db81aeeadbe5bad8fce31227a00
Tomasz Latkowski <t.latkowski@gmail.com>
2018-01-09 17:54:33 +0100

    fixed multihead attention, added summaries

--- models/sdot_product_attention.py
+++ models/sdot_product_attention.py
@@ -16,13 +16,11 @@ class ScaledDotProductAttentionSiameseNet:
             embedded_x2 = tf.gather(word_embeddings, self.x2)
 
         with tf.variable_scope('siamese-multihead-attention'):
+            self.out1 = multihead_attention(embedded_x1, embedded_x1, embedded_x1, reuse=False)
+            self.out2 = multihead_attention(embedded_x2, embedded_x2, embedded_x2)
 
-            self.out1 = multihead_attention(embedded_x1, embedded_x1, embedded_x1, 512)
-            tf.get_variable_scope().reuse_variables()
-            self.out2 = multihead_attention(embedded_x2, embedded_x2, embedded_x2, 512)
-
-            self.out1 = tf.reduce_mean(self.out1, axis=1)
-            self.out2 = tf.reduce_mean(self.out2, axis=1)
+            self.out1 = tf.reduce_sum(self.out1, axis=1)
+            self.out2 = tf.reduce_sum(self.out2, axis=1)
 
             self.predictions = manhattan_similarity(self.out1, self.out2)
 
@@ -32,8 +30,12 @@ class ScaledDotProductAttentionSiameseNet:
 
         with tf.variable_scope('metrics'):
             self.temp_sim = tf.rint(self.predictions)
-            self.correct_predictions = tf.equal(self.temp_sim, tf.cast(self.labels, 'float'))
-            self.acc = tf.reduce_mean(tf.cast(self.correct_predictions, 'float'))
+            self.correct_predictions = tf.equal(self.temp_sim, tf.to_float(self.labels))
+            self.accuracy = tf.reduce_mean(tf.to_float(self.correct_predictions))
+
+            tf.summary.scalar("loss", self.loss)
+            tf.summary.scalar("accuracy", self.accuracy)
+            self.summary_op = tf.summary.merge_all()
 
 
 def scaled_dot_product_attention(queries, keys, values, dk_size):
@@ -42,14 +44,25 @@ def scaled_dot_product_attention(queries, keys, values, dk_size):
     return tf.nn.softmax(v_q_k)
 
 
-def multihead_attention(queries, keys, values, dk_size, num_heads=8):
-    Q = tf.concat(tf.split(queries, num_heads, axis=2), axis=0)
-    K = tf.concat(tf.split(keys, num_heads, axis=2), axis=0)
-    V = tf.concat(tf.split(values, num_heads, axis=2), axis=0)
+def multihead_attention(queries, keys, values, num_units=None, num_heads=8, reuse=True):
+    with tf.variable_scope('multihead-attention', reuse=reuse):
+        if num_units is None:
+            num_units = queries.get_shape().as_list()[-1]
+        Q = tf.layers.dense(queries, num_units)
+        K = tf.layers.dense(keys, num_units)
+        V = tf.layers.dense(values, num_units)
 
-    Q_K = tf.nn.softmax(tf.matmul(Q, tf.transpose(K, [0, 2, 1])))
-    Q_K_V = tf.matmul(Q_K, V)
-    Q_K_V_ = tf.concat(tf.split(Q_K_V, num_heads, axis=0), axis=2)
+        Q = tf.concat(tf.split(Q, num_heads, axis=2), axis=0)
+        K = tf.concat(tf.split(K, num_heads, axis=2), axis=0)
+        V = tf.concat(tf.split(V, num_heads, axis=2), axis=0)
 
-    logits = tf.layers.dense(Q_K_V_, 512)
+        Q_K = tf.matmul(Q, tf.transpose(K, [0, 2, 1]))
+        model_size = tf.to_float(Q.get_shape().as_list()[-1])
+
+        Q_K = tf.nn.softmax(Q_K / tf.sqrt(model_size))
+
+        Q_K_V = tf.matmul(Q_K, V)
+        Q_K_V_ = tf.concat(tf.split(Q_K_V, num_heads, axis=0), axis=2)
+
+        logits = tf.layers.dense(Q_K_V_, num_units)
     return logits
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tqdm import tqdm
 
 from data.data_loader import load_snli
-from models.lstm import LSTMBasedSiameseNet
+from models.sdot_product_attention import ScaledDotProductAttentionSiameseNet
 
 data_fn = 'train_snli.txt'
 logs_path = 'logs/'
@@ -30,14 +30,17 @@ eval_sen2 = sen2[-1000:]
 eval_labels = labels[-1000:]
 
 with tf.Session() as session:
-    model = LSTMBasedSiameseNet(max_doc_len, vocabulary_size, embedding_size, hidden_size)
+    model = ScaledDotProductAttentionSiameseNet(max_doc_len, vocabulary_size, embedding_size, hidden_size)
+    global_step = 0
+
     init = tf.global_variables_initializer()
     init_local = tf.local_variables_initializer()
     session.run(init)
     session.run(init_local)
     if not os.path.isdir(logs_path):
         os.makedirs(logs_path)
-    summary_writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
+    summary_writer = tf.summary.FileWriter(logs_path, graph=session.graph)
+
     metrics = {'acc': 0.0}
     for epoch in tqdm(range(num_epochs), desc="Epochs"):
         tqdm_iter = tqdm(range(num_batches),
@@ -46,6 +49,7 @@ with tf.Session() as session:
                                 leave=False,
                                 postfix=metrics)
         for batch in tqdm_iter:
+            global_step += 1
             x1_batch = train_sen1[batch * batch_size:(batch + 1) * batch_size]
             x2_batch = train_sen2[batch * batch_size:(batch + 1) * batch_size]
             y_batch = train_labels[batch * batch_size:(batch+1) * batch_size]
@@ -53,8 +57,9 @@ with tf.Session() as session:
             loss, _ = session.run([model.loss, model.opt], feed_dict=feed_dict)
             if batch % eval_every == 0:
                 feed_dict = {model.x1: eval_sen1, model.x2: eval_sen2, model.labels: eval_labels}
-                acc = session.run([model.acc], feed_dict=feed_dict)
-                tqdm_iter.set_postfix(acc=acc)
+                accuracy, summary = session.run([model.accuracy, model.summary_op], feed_dict=feed_dict)
+                summary_writer.add_summary(summary, global_step)
+                tqdm_iter.set_postfix(acc=accuracy)
 
 
 
