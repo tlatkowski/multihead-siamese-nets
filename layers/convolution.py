b6af5e946ceeeb6a463f67c9044e62ae4909e95c
Tomasz Latkowski <t.latkowski@gmail.com>
2018-02-17 11:20:28 +0100

    changes in cnn and reqs

--- layers/convolution.py
+++ layers/convolution.py
@@ -9,6 +9,7 @@ def cnn_layer(embedded_x, max_seq_len, num_filters, filter_size, reuse=False):
                                       filters=num_filters,
                                       kernel_size=[filter_size, embedding_dim],
                                       activation=tf.nn.relu)
+    with tf.variable_scope('pooling', reuse=reuse):
         pooling = tf.layers.max_pooling2d(convoluted,
                                           pool_size=[max_seq_len - filter_size + 1, 1],
                                           strides=[1, 1])
@@ -18,8 +19,10 @@ def cnn_layer(embedded_x, max_seq_len, num_filters, filter_size, reuse=False):
 
 def cnn_layers(embedded_x, max_seq_len, num_filters, filter_sizes, reuse=False):
     pooled_flats = []
-    for i, (n, size) in enumerate(zip(num_filters, filter_sizes)):
-        with tf.variable_scope('cnn_layer_{}'.format(i), reuse=reuse):
-            pooled_flat = cnn_layer(embedded_x, max_seq_len, num_filters=n, filter_size=size, reuse=reuse)
-            pooled_flats.append(pooled_flat)
-    return tf.concat(pooled_flats, axis=1)
+    with tf.variable_scope('cnn_network', reuse=reuse):
+        for i, (n, size) in enumerate(zip(num_filters, filter_sizes)):
+            with tf.variable_scope('cnn_layer_{}'.format(i), reuse=reuse):
+                pooled_flat = cnn_layer(embedded_x, max_seq_len, num_filters=n, filter_size=size, reuse=reuse)
+                pooled_flats.append(pooled_flat)
+        cnn_output = tf.concat(pooled_flats, axis=1)
+    return cnn_output
