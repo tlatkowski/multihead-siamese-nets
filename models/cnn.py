d33124b7fc50335e22a9a9695b26ea97a3bf887a
Tomasz Latkowski <tomasz.latkowski@tcl.com>
2019-04-26 20:57:11 +0200

    added dropout to models

--- models/cnn.py
+++ models/cnn.py
@@ -3,7 +3,6 @@ from layers.losses import mse
 from layers.similarity import manhattan_similarity
 from models.base_model import BaseSiameseNet
 from utils.config_helpers import parse_list
-from layers.basics import dropout
 
 
 class CnnSiameseNet(BaseSiameseNet):
@@ -14,19 +13,21 @@ class CnnSiameseNet(BaseSiameseNet):
     def siamese_layer(self, sequence_len, model_cfg):
         num_filters = parse_list(model_cfg['PARAMS']['num_filters'])
         filter_sizes = parse_list(model_cfg['PARAMS']['filter_sizes'])
+        dropout_rate = float(model_cfg['PARAMS']['dropout_rate'])
 
         out1 = cnn_layers(self.embedded_x1,
                           sequence_len,
                           num_filters=num_filters,
-                          filter_sizes=filter_sizes)
+                          filter_sizes=filter_sizes,
+                          is_training=self.is_training,
+                          dropout_rate=dropout_rate)
 
         out2 = cnn_layers(self.embedded_x2,
                           sequence_len,
                           num_filters=num_filters,
                           filter_sizes=filter_sizes,
+                          is_training=self.is_training,
+                          dropout_rate=dropout_rate,
                           reuse=True)
-
-        out1 = dropout(out1, self.is_training)
-        out2 = dropout(out2, self.is_training)
 
         return manhattan_similarity(out1, out2)
