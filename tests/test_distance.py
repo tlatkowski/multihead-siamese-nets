1165a80b199df65dc506343f6a4c52d12f606dfb
Tomasz Latkowski <tlatkowski@gmail.com>
2019-11-24 13:30:48 +0100

    corrected tests

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -7,7 +7,7 @@ from layers.similarity import manhattan_distance, cosine_distance, euclidean_dis
 class TestDistance(tf.test.TestCase):
 
     def testManhattanDistance(self):
-        with self.test_session() as test_session:
+        with self.cached_session() as test_session:
             x1 = np.array([[1., 2., 3.]])
             x2 = np.array([[3., 2., 1.]])
 
@@ -17,7 +17,7 @@ class TestDistance(tf.test.TestCase):
             self.assertEqual(actual_output, correct_output)
 
     def testManhattanDistanceSame(self):
-        with self.test_session() as test_session:
+        with self.cached_session() as test_session:
             x1 = np.array([[1., 1.]])
             x2 = np.array([[1., 1.]])
 
@@ -27,7 +27,7 @@ class TestDistance(tf.test.TestCase):
             self.assertEqual(actual_output, correct_output)
 
     def testManhattanDistance2D(self):
-        with self.test_session() as test_session:
+        with self.cached_session() as test_session:
             x1 = np.array([[1., 1., 1.], [2., 2., 2.]])
             x2 = np.array([[2., 2., 2.], [1., 1., 1.]])
 
@@ -37,7 +37,7 @@ class TestDistance(tf.test.TestCase):
             self.assertAllEqual(actual_output, correct_output)
 
     def testCosineDistanceOpposite(self):
-        with self.test_session() as test_session:
+        with self.cached_session() as test_session:
             x1 = np.array([[-1., -1.]])
             x2 = np.array([[1., 1.]])
 
@@ -47,7 +47,7 @@ class TestDistance(tf.test.TestCase):
             self.assertAllClose(actual_output, correct_output)
 
     def testCosineDistanceSame(self):
-        with self.test_session() as test_session:
+        with self.cached_session() as test_session:
             x1 = np.array([[1., 1.]])
             x2 = np.array([[1., 1.]])
 
@@ -57,7 +57,7 @@ class TestDistance(tf.test.TestCase):
             self.assertAllClose(actual_output, correct_output)
 
     def testCosineDistanceOrthogonal(self):
-        with self.test_session() as test_session:
+        with self.cached_session() as test_session:
             x1 = np.array([[-1., 1.]])
             x2 = np.array([[1., 1.]])
 
@@ -67,7 +67,7 @@ class TestDistance(tf.test.TestCase):
             self.assertEqual(actual_output, correct_output)
 
     def testCosineDistance2D(self):
-        with self.test_session() as test_session:
+        with self.cached_session() as test_session:
             x1 = np.array([[0., 1.], [2., 3.]])
             x2 = np.array([[0., 1.], [-2., -3.]])
 
@@ -77,7 +77,7 @@ class TestDistance(tf.test.TestCase):
             self.assertAllClose(actual_output, correct_output)
 
     def testEuclideanDistance(self):
-        with self.test_session() as test_session:
+        with self.cached_session() as test_session:
             x1 = np.array([[3., 1.]])
             x2 = np.array([[1., 1.]])
 
--- tests/test_similarity.py
+++ tests/test_similarity.py
@@ -7,7 +7,7 @@ from models.rnn import manhattan_similarity
 class TestSimilarity(tf.test.TestCase):
 
     def testManhattanSimilaritySame(self):
-        with self.test_session() as test_session:
+        with self.cached_session() as test_session:
             x1 = np.array([[1., 1.]])
             x2 = np.array([[1., 1.]])
             siamese_lstm_model = manhattan_similarity(x1, x2)
@@ -17,7 +17,7 @@ class TestSimilarity(tf.test.TestCase):
             self.assertEqual(actual_output, correct_output)
 
     def testSimilarity2D(self):
-        with self.test_session() as test_session:
+        with self.cached_session() as test_session:
             x1 = np.array([[1., 1.], [1., 1.]])
             x2 = np.array([[1., 1.], [1., 1.]])
             siamese_lstm_model = manhattan_similarity(x1, x2)
